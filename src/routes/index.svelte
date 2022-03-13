cc7044a6695d31cd5837877def8cd431fa45f17e
Peter Parente <parente@gmail.com>
2022-03-12 23:28:44 -0500

    Rough in the calculation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,69 @@
 <script type="ts">
-  let frequency: string = "1";
-  let frequency_unit: string = "hour";
-  let optimization: string = "1";
-  let optimization_unit: string = "minute";
-  let optimization_plural: boolean = false;
-  let investment: number = 5;
-  let investment_unit: string = "hours";
-  let lifetime: number = 5;
-  let lifetime_unit: string = "years";
+  let tasks: string = "1";
+  let frequencyUnit: number = 604800;
+  let savings: string = "30";
+  let savingsUnit: number = 1;
+  let investment: string;
+  let investmentUnit: string;
+  const lifetime: number = 5;
+  const lifetimeUnit: string = "years";
 
-  const optimization_units = [
-    "second",
-    "minute",
-    "hour",
-    "day",
-    "week"
+  const units = [
+    {value: 1, singular: "second", plural: "seconds"},
+    {value: 60, singular: "minute", plural: "minutes"},
+    {value: 3600, singular: "hour", plural: "hours"},
+    {value: 86400, singular: "day", plural: "days"},
+    {value: 604800, singular: "week", plural: "weeks"},
+    {value: 2592000, singular: "month", plural: "months"},
+    {value: 7776000, singular: "quarter", plural: "quarters"},
+    {value: 31536000, singular: "year", plural: "years"},
 	];
 
-  // Correct pluralization of optimization unit options
-  $: if(optimization === "1" && optimization_plural) {
-    optimization_plural = false;
-    optimization_unit = optimization_unit.substring(0, optimization_unit.length-1);
-  } else if(optimization !== "1" && !optimization_plural) {
-    optimization_unit += "s";
-    optimization_plural = true;
+  function unitValueToLabel(value: string, unitValue: number): string {
+    for(let i=0; i < units.length; i++) {
+      if(units[i].value === unitValue) {
+        return (value === "1") ? units[i].singular : units[i].plural;
+      }
+    }
+    throw new Error("unexpected unit value");
+  }
+
+  function unitLabelToValue(label: string) {
+    for(let i=0; i < units.length; i++) {
+      if(units[i].singular === label || units[i].plural === label) {
+        return units[i].value;
+      }
+    }
+    throw new Error("unexpected unit label");
+  }
+
+  const secondsPerLifetime = lifetime * unitLabelToValue(lifetimeUnit);
+
+  /* 
+   * tasks         | secondsPerLifetime | savings | savingsUnit |
+   * ------------- | ------------------ | ------- | ------------|
+   * frqeuencyUnit |                    | 1 task  |             |
+   */
+  $: {
+    const limit = Number(tasks) / frequencyUnit * secondsPerLifetime * Number(savings) * savingsUnit;
+    // Invalid value in one of the fields
+    if(isNaN(limit)) {
+      investment = '???';
+    } else {
+      for(let i=0; i <= units.length; i++) {
+        if((i === units.length) || (units[i].value > limit)) {
+          if(i === 0) {
+            // Less than a second savings
+            investment = '???';
+          } else {
+            // Use the largest unit that results in a value > 1
+            investment = (limit / units[i-1].value).toFixed(1);
+            investmentUnit = (investment === "1") ? units[i-1].singular : units[i-1].plural;
+          }
+          break;
+        }
+      }
+    }
   }
 </script>
 
@@ -43,56 +83,48 @@
     <h1 class="mb-1">Is it worth the time?</h1>
     <small>A calculator based on <a href="https://xkcd.com/1205/">xkcd #1205</a></small>
 
-    <form on:submit|preventDefault>
-      <p>We perform a routine task 
-        <input type="text" bind:value={frequency} required autofocus class="form-input 
-          w-16
-          bg-transparent
-          border-0 border-b-2 border-sky-300
-          focus:ring-0 focus:border-sky-500
-          px-3 py-0
-          text-sm"/> 
-        {frequency === "1" ? "time" : "times"} every
-        <select bind:value={frequency_unit} class="form-select
-          w-28
-          bg-transparent
-          border-0 border-b-2 border-sky-300
-          focus:ring-0 focus:border-sky-500
-          px-3 py-0
-          text-sm">
-          <option>hour</option>
-          <option>day</option>
-          <option>week</option>
-          <option>month</option>
-          <option>quarter</option>
-          <option>year</option>
-        </select>. We think we can save <input type="text" bind:value={optimization} required class="form-input 
-          w-16
-          bg-transparent
-          border-0 border-b-2 border-emerald-300
-          focus:ring-0 focus:border-emerald-500
-          px-3 py-0
-          text-sm" />
-        <select bind:value={optimization_unit} class="form-select
-          w-28
-          bg-transparent
-          border-0 border-b-2 border-emerald-300
-          focus:ring-0 focus:border-emerald-500
-          px-3 py-0
-          text-sm">
-          {#each optimization_units as unit}
-            <option>{optimization === "1" ? unit : `${unit}s`}</option>
-          {/each}
-        </select> each time we perform this task if we invest time in optimizing it.
-      </p>
+    <p>We perform a routine task 
+      <input type="text" bind:value={tasks} required autofocus class="form-input 
+        w-16
+        bg-transparent
+        border-0 border-b-2 border-sky-300
+        focus:ring-0 focus:border-sky-500
+        px-3 py-0"/> 
+      {tasks === "1" ? "time" : "times"} every
+      <select bind:value={frequencyUnit} class="form-select
+        w-28
+        bg-transparent
+        border-0 border-b-2 border-sky-300
+        focus:ring-0 focus:border-sky-500
+        px-3 py-0">
+        {#each units as unit}
+          <option value={unit.value}>{unit.singular}</option>
+        {/each}
+      </select>. We think we can save <input type="text" bind:value={savings} required class="form-input 
+        w-16
+        bg-transparent
+        border-0 border-b-2 border-emerald-300
+        focus:ring-0 focus:border-emerald-500
+        px-3 py-0" />
+      <select bind:value={savingsUnit} class="form-select
+        w-28
+        bg-transparent
+        border-0 border-b-2 border-emerald-300
+        focus:ring-0 focus:border-emerald-500
+        px-3 py-0">
+        {#each units as unit}
+          <option value={unit.value}>{savings === "1" ? unit.singular : unit.plural}</option>
+        {/each}
+      </select> each time we perform this task if we invest time in optimizing it.
+    </p>
 
-      <p>If we want to recoup our investment by saving <span class="underline decoration-2
-      decoration-emerald-300">{optimization} {optimization_unit}</span> per task over <span
-      class="underline decoration-2 decoration-chrome-300">{lifetime} {lifetime_unit}</span>, we can spend no more
-      than <strong class="underline decoration-2 decoration-rose-300">{investment} {investment_unit}</strong>
-      performing the optimization.</p>
-      
-      <p>Should we invest our time?</p>
-    </form>
+    <p>If we want to recoup our investment by saving <span class="underline decoration-2
+    decoration-emerald-300">{savings} {unitValueToLabel(savings,
+    savingsUnit)}</span> per task over <span class="underline decoration-2
+    decoration-chrome-300">{lifetime} {lifetimeUnit}</span>, we can spend no more than <strong
+    class="underline decoration-2 decoration-rose-300">{investment} {investmentUnit}</strong>
+    performing the optimization.</p>
+    
+    <p>Should we invest our time?</p>
   </div>
 </div>
